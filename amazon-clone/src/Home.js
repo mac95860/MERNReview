fb2da383e76aedf45904c9aa4aa48c699db2a155
Michael Cervantes <mac95860@hotmail.com>
2020-11-21 11:58:02 -0600

    playing with props

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -9,18 +9,22 @@ function Home() {
                <img className = "home_image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" alt ="" />            
            
                <div className= "home_row">
+                    <Product 
+                        title= "The Lean Startup"
+                        price = {29.99}
+                        image= "https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+                        rating={5}
+                    />
                     <Product/>
-                    
                </div>
 
                <div className = "home_row">
-                    {/* product */}
-                    {/* product */}
-                    {/* product */}
+                    <Product/>
+                    <Product/>
+                    <Product/>
                </div>
                <div className = "home_row">
-                    {/* product */} 
-                    {/* product */}
+                    <Product/>
                </div>
 
            </div> 
