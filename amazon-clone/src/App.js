d053566071024fd590fb9de24e15b5be61770841
Michael Cervantes <mac95860@hotmail.com>
2020-11-20 18:38:15 -0600

    nav style finished

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
@@ -7,6 +8,8 @@ function App() {
       {/* header */}
       <Header/>
       {/* Home */}
+
+      <Home/>
       {/*  */}
     </div>
   );
--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 
 function Header() {
     return (
@@ -16,18 +17,23 @@ function Header() {
             <div className="header_nav">
                 <div className = "header_option">
                     {/* signIn */}
-                    <span className ="header_optionLineONe">Hello Guest</span>
+                    <span className ="header_optionLineOne">Hello Guest</span>
                     <span className ="header_optionLineTwo">SignIn</span>
                 </div>
                 <div className = "header_option">
                     {/*  Orders*/}
-                    <span className ="header_optionLineONe">returns</span>
+                    <span className ="header_optionLineOne">returns</span>
                     <span className ="header_optionLineTwo"> Orders</span>
                 </div>
                 <div className = "header_option">
                     {/* PrimeMembership */}
-                    <span className ="header_optionLineONe">Your</span>
+                    <span className ="header_optionLineOne">Your</span>
                     <span className ="header_optionLineTwo">Prime</span>
+                </div>
+
+                <div className = "header_optionBasket">
+                    <ShoppingBasketIcon/>
+                    <span className = "header_optionLineTwo header_basketCount">0</span> 
                 </div>
             </div>
         </div>
