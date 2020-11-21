d9dc7576c55ddf12cd3367c7cfe24f45b50fc671
Michael Cervantes <mac95860@hotmail.com>
2020-11-21 11:28:44 -0600

    banner and product card

---
+++ amazon-clone/src/components/Product/Product.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Product.css";
+
+export default function Product() {
+    return (
+        <div className = "product">
+            <div className="product_info">
+                <p>The Lean Startup</p>
+                <p className = "product_price">
+                    <small>$</small>
+                    <strong>19.99</strong>
+                </p>
+                <div className = "product_rating">
+                    <p>⭐</p>
+                </div>
+            </div>
+            <img src = "https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg" alt = ""/>
+            <button>Add to Basket</button>
+        </div>
+    )
+}
