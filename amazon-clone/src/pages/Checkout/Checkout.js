680c3417f0842437f3f302ca99661a53364c2350
Michael Cervantes <mac95860@hotmail.com>
2020-11-22 20:49:10 -0600

    last commit for the night

--- amazon-clone/src/pages/Checkout/Checkout.js
+++ amazon-clone/src/pages/Checkout/Checkout.js
@@ -5,7 +5,9 @@ import "./Checkout.css";
 export default function Checkout() {
     return (
         <div className = "checkout">
-            <h1>Checkout Page</h1>
+            <div className="checkout_left">
+                <img className='checkout_ad' src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt=""/>
+            </div>
         </div>
     )
 }
