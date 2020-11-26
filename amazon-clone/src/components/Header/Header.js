378b980feb39d62f20948b487c66a359a28621d9
Michael Cervantes <mac95860@hotmail.com>
2020-11-25 22:37:11 -0600

    applying react currency format

--- amazon-clone/src/components/Header/Header.js
+++ amazon-clone/src/components/Header/Header.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import { Link } from "react-router-dom";
 
 function Header() {
     return (
         <div className = "header">
-            <img className = "header_logo" src = '/images/Amazonlogo.png' alt = "logo"/>
-    
-
+            <Link to="/">
+                <img className = "header_logo" src = '/images/Amazonlogo.png' alt = "logo"/>
+            </Link>
+            
             <div className = "header_search">
                 <input className ="header_searchInput" type = "text"/>
                 <SearchIcon className = "header_searchIcon"/>
@@ -30,11 +32,14 @@ function Header() {
                     <span className ="header_optionLineOne">Your</span>
                     <span className ="header_optionLineTwo">Prime</span>
                 </div>
+                <Link to = "/checkout">
+                    <div className = "header_optionBasket">
+                        <ShoppingBasketIcon/>
+                        <span className = "header_optionLineTwo header_basketCount">0</span> 
+                    </div>
+                </Link>
 
-                <div className = "header_optionBasket">
-                    <ShoppingBasketIcon/>
-                    <span className = "header_optionLineTwo header_basketCount">0</span> 
-                </div>
+                
             </div>
         </div>
     )
