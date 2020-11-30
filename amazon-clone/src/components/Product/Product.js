a35386fbef258258500ca8cd63c8a1a26ccd24f2
Michael Cervantes <mac95860@hotmail.com>
2020-11-29 19:40:51 -0600

    add to cart function working

--- amazon-clone/src/components/Product/Product.js
+++ amazon-clone/src/components/Product/Product.js
@@ -1,7 +1,23 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "../../util/StateProvider"
 
-export default function Product({title, image, price, rating}) {
+export default function Product({id, title, image, price, rating}) {
+
+    const [{basket}, dispatch] = useStateValue();
+    console.log("this is the basket >>>", basket);
+    const addToBasket = () => {
+            dispatch({
+                type: "ADD_TO_BASKET",
+                item: {
+                    id: id,
+                    title: title,
+                    image: image,
+                    price: price,
+                    rating: rating
+                }
+            })
+    }
     return (
         <div className = "product">
             <div className="product_info">
@@ -19,7 +35,7 @@ export default function Product({title, image, price, rating}) {
                 </div>
             </div>
             <img src = {image} alt = ""/>
-            <button>Add to Basket</button>
+            <button onClick={addToBasket}>Add to Basket</button>
         </div>
     )
 }
