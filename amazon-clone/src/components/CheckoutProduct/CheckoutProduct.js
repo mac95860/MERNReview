f3102b724b652c75b7dadcec95e4d4fe9c7a2a21
Michael Cervantes <mac95860@hotmail.com>
2020-12-01 19:10:00 -0600

    checkout product styling

--- amazon-clone/src/components/CheckoutProduct/CheckoutProduct.js
+++ amazon-clone/src/components/CheckoutProduct/CheckoutProduct.js
@@ -4,19 +4,20 @@ import './CheckoutProduct.css'
 export default function CheckoutProduct({id, image, title, price, rating}) {
     return (
         <div className = "checkoutProduct">
-            <img className = "checkoutProduct_image" src = {image} alt = ""/>
-            <div className = 'checkoutProduct_info'>
-                <p className = "checkoutProduct_title">{title}</p>
-                <p className = "checkoutProduct_price">
-                    <small>$</small>
-                    <strong>{price}</strong>
+            <img className='checkoutProduct_image' src={image} />
+
+            <div className='checkoutProduct_info'>
+                <p className='checkoutProduct_title'>{title}</p>
+                <p className="checkoutProduct_price">
+                        <small>$</small>
+                        <strong>{price}</strong>
                 </p>
-                <div className = "checkoutProduct_rating">
-                    {Array(rating)
-                    .fill()
-                    .map((_, i) => {
-                        <p>⭐</p>
-                    })}
+                <div className="checkoutProduct_rating">
+                        {Array(rating)
+                        .fill()
+                        .map((_, i) => (
+                            <p>⭐</p>
+                        ))}
                 </div>
                 <button>Remove from basket</button>
             </div>
