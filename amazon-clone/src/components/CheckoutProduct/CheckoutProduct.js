c222701ed4ae58dd97972e37281ce9b0539a04f4
Michael Cervantes <mac95860@hotmail.com>
2020-12-01 18:56:40 -0600

    display basket items

---
+++ amazon-clone/src/components/CheckoutProduct/CheckoutProduct.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './CheckoutProduct.css'
+
+export default function CheckoutProduct({id, image, title, price, rating}) {
+    return (
+        <div className = "checkoutProduct">
+            <img className = "checkoutProduct_image" src = {image} alt = ""/>
+            <div className = 'checkoutProduct_info'>
+                <p className = "checkoutProduct_title">{title}</p>
+                <p className = "checkoutProduct_price">
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className = "checkoutProduct_rating">
+                    {Array(rating)
+                    .fill()
+                    .map((_, i) => {
+                        <p>⭐</p>
+                    })}
+                </div>
+                <button>Remove from basket</button>
+            </div>
+        </div>
+    )
+}
--- amazon-clone/src/pages/Checkout/Checkout.js
+++ amazon-clone/src/pages/Checkout/Checkout.js
@@ -1,24 +1,34 @@
 import React from 'react';
 import "./Checkout.css";
 import Subtotal from '../../components/Subtotal/Subtotal'
+import CheckoutProduct from '../../components/CheckoutProduct/CheckoutProduct'
+import {useStateValue} from '../../util/StateProvider'
 
 
 export default function Checkout() {
+    const [{basket}, dispatch] = useStateValue();
     return (
         <div className = "checkout">
             <div className="checkout_left">
                 <img className='checkout_ad' src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt=""/>
                 <div>
                     <h2 className = "checkout_title">Your Shopping Basket</h2>
-
-                        {/* Checkout Products  */}
+                    {basket.map(item => (
+                        <CheckoutProduct
+                        id={item.id}
+                        title={item.title}
+                        image={item.image}
+                        price = {item.price}
+                        rating={item.rating}
+                       />
+                    ))}
+                       
                 </div>
             </div>
 
             <div className = "checkout_right">
                 <Subtotal/>
             </div>
-            
         </div>
     )
 }
