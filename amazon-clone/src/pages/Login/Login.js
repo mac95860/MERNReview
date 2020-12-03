09227435bd5d0e77262bf820fa65bbdb0221bd01
Michael Cervantes <mac95860@hotmail.com>
2020-12-03 17:49:34 -0600

    ready for backend user authentication

--- amazon-clone/src/pages/Login/Login.js
+++ amazon-clone/src/pages/Login/Login.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Login.css'
 import { Link } from 'react-router-dom';
 
 export default function Login() {
+    const signIn = e => {
+        e.preventDefault();
+
+        //firebase implementation goes here
+    }
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const register = e => {
+        e.preventDefault();
+
+        //FIREBASE IMPLEMENTATION GOES HERE
+    }
     return (
         <div className = 'login'>
             <Link to = '/'>
@@ -13,18 +26,18 @@ export default function Login() {
                 <h1>Sign in</h1>
                 <form>
                     <h5>E-mail</h5>
-                    <input type="text" />
+                    <input type="text" value={email} onChange={e => setEmail(e.target.value)}/>
                     
                     <h5>Password</h5>
-                    <input type="password" />
-                    <button>Sign in</button>
+                    <input type="password"  value = {password} onChange = {e => setPassword(e.target.value)}/>
+                    <button type="submit" className = 'login-signInButton' onClick={signIn}>Sign in</button>
 
                 </form>
 
                 <p>
                     By signing-in you agree to the AMAZON FAKE Conditions of Use & Sale. Please see our Privacy NOtic, our Cookies Notice and our Interest-Based Adds
                 </p>
-                <button>Create your amazon account</button>
+                <button onClick = {register} className='login_registerButton'>Create your amazon account</button>
             </div>
         </div>
     )
