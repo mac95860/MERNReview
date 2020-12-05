c9f1708d63c7b29417ef4850b5f61297e107058a
Michael Cervantes <mac95860@hotmail.com>
2020-12-05 13:46:02 -0600

    more hooks with firebase

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,12 +1,29 @@
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
 import Checkout from './pages/Checkout/Checkout';
 import Login from './pages/Login/Login'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { auth } from './firebase';
 
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    //as soon as app loads, this method starts listening
+    auth.onAuthStateChanged(authUser => {
+      console.log('THE USER IS >>> '. authUser);
+      if(authUser) {
+        //user just logged in
+
+      } else {
+        //user is logged out
+      }
+    })
+  }, [])
+
   return (
     <Router>
       <div className="App">
--- amazon-clone/src/util/reducer.js
+++ amazon-clone/src/util/reducer.js
@@ -1,5 +1,6 @@
 export const initialState = {
     basket: [],
+    user: null
 }
 
 export const getBasketTotal = (basket) =>  basket?.reduce((amount, item) => item.price + amount, 0 );
