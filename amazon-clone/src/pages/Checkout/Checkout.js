bca6955fc9e6e9a85dba62fba60173ced3a5d909
Michael Cervantes <mac95860@hotmail.com>
2020-12-08 21:49:20 -0600

    test

--- amazon-clone/src/pages/Checkout/Checkout.js
+++ amazon-clone/src/pages/Checkout/Checkout.js
@@ -13,7 +13,7 @@ export default function Checkout() {
                 <img className='checkout_ad' src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt=""/>
                 <div>
                     <h3>Hello, {user?.email}</h3>
-                    <h2 className = "checkout_title">Your Shopping Basket</h2>
+                    <h2 className = "checkout_title">Your Shopping Basket!</h2>
                     {basket.map(item => (
                         <CheckoutProduct
                         id={item.id}
