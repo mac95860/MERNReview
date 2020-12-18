1747443757fdad37d96b77159716230d2234fbea
Michael Cervantes <mac95860@hotmail.com>
2020-12-17 21:35:17 -0600

    more work with routes

--- amazon-clone/functions/index.js
+++ amazon-clone/functions/index.js
@@ -12,6 +12,19 @@ app.get('/', (req, res) => {
     res.status(200).send('hello world')
 });
 
+app.post('/payments/create', async (req, res) =>{
+    const total = req.query.total;
+
+    console.log('Payment request recieved', total);
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: total, //in sub units of currency
+        currency: 'usd'
+    })
+    res.status(201).send({
+        clientSecret: paymentIntent.client_secret,
+    })
+})
+
 exports.api = functions.https.onRequest(app);
 
 
