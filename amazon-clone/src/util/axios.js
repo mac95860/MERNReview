f76e2ab23e8cef4ee7a262ebdcb10316452a6005
Michael Cervantes <mac95860@hotmail.com>
2021-01-06 13:30:07 -0600

    more deployment issues

--- amazon-clone/src/util/axios.js
+++ amazon-clone/src/util/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/challenge-719b6/us-central1/api' //API (cloud function)
+    baseURL: 'https://us-central1-challenge-719b6.cloudfunctions.net/api'
+        //'http://localhost:5001/challenge-719b6/us-central1/api' //API (cloud function) test url
 });
 
 export default instance;
